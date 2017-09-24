c414c6434ed105273455cd5e4feec59469125909
viniloreto <vini.loreto@gmail.com>
2017-09-24 17:16:33 -0500

    Criacao da classe fornecedor

---
+++ src/java/br/com/fatecpg/oo/Fornecedor.java
@@ -0,0 +1,60 @@
+
+package br.com.fatecpg.oo;
+
+
+public class Fornecedor {
+    private String nome;
+    private String razaosocial;
+    private String cnpj;        
+    private String email;
+    private String telefone;
+    private String endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRazaosocial() {
+        return razaosocial;
+    }
+
+    public void setRazaosocial(String razaosocial) {
+        this.razaosocial = razaosocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
